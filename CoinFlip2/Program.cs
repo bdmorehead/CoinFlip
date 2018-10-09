cde385f379f63325b76ad942c6c5749ed06faaad
Bobby Morehead <bobby.morehead@outlook.com>
2018-10-09 12:24:37 -0400

    9 Oct Update

--- CoinFlip2/Program.cs
+++ CoinFlip2/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoinFlip
+namespace CoinFlip2
 {
     class Program
     {
@@ -12,34 +12,36 @@ namespace CoinFlip
         {
             int coin;
             string userGuess;
+            string coinDescription;
+
             Random rnd = new Random();
             // Query the user
             Console.Write("Enter your guess, Heads (H) or Tails(T): ");
-                userGuess = Console.ReadLine();
+            userGuess = Console.ReadLine();
 
             // Get a random number for the coin flip
 
             coin = rnd.Next(0, 2);
-
-            if (coin == 0 && userGuess == "T")
+            if (coin == 1)
             {
-                Console.WriteLine("The coin flip was tails, you win");
-            }
-            if (coin == 1 && userGuess == "H")
-            {
-                Console.WriteLine("The coin flie was heads, you win");
+                coinDescription = "Heads";
             }
             else
             {
-                if (coin == 0)
-                {
-                    Console.WriteLine("It was heads, you lose");
-                }
-                if (coin == 1)
-                {
-                    Console.WriteLine("It was tails, you lose");
-                }
+                coinDescription = "Tails";
+            }
 
+            if ((coin == 0 && userGuess == "T") || (coin == 1 && userGuess == "H"))
+            {
+                Console.WriteLine($"The coin flip was {0}, you win", coinDescription);
+            }
+
+            else
+            {
+                if ((coin == 0) || (coin == 1))
+                {
+                    Console.WriteLine($"It was {coinDescription}, you lose");
+                }
             }
         }
     }
